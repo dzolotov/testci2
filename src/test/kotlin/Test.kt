ef91eb4e13d8549bc1736fbbef9e2041ca75c19f
Dmitrii Zolotov <dmitrii.zolotov@gmail.com>
2022-10-14 23:53:30 +0600

    Initial commit

---
+++ src/test/kotlin/Test.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MyTest {
+    @Test
+    fun testSum() {
+        assertEquals(4, sum(2,2))
+    }
+}
